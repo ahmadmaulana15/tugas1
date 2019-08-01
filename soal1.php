e29dfbeca555faf0524283e4cea1e9e633baf3f3
jay150503maulana <fajarpro27@gmail.com>
2019-08-01 16:34:19 +0700

    saya menambahkan project 1

---
+++ soal1.php
@@ -0,0 +1,11 @@
+<?php
+$kilo =15000;
+$dus =6*5;
+$jdus =12000;
+$hasil = $kilo*$dus;
+$hasil1 = ($kilo*$dus)+$jdus;
+
+
+echo "hasil penjualan semua jambu adalah Rp.$hasil<br>";
+echo "hasil semua penjualan adalah Rp.$hasil1";
+?>
