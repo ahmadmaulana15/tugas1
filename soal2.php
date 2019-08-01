db20eedebadc748570a973c672a0eb6603c89b0b
jay150503maulana <fajarpro27@gmail.com>
2019-08-01 16:59:20 +0700

    saya menambahkan project kedua

---
+++ soal2.php
@@ -0,0 +1,22 @@
+<?php
+$tama = 1;
+$toni = 0.5;
+$jtinitama = 14;
+$jtinitoni = 34;
+
+
+echo "umur tama = $jtama X<br>";
+echo "umur toni = $toni X <br>";
+echo "umur toni +umur tini = $jtinitoni <br>";
+$x = $tama + $toni;
+$y = $jtinitama+$jtinitoni;
+$z = $y/$x;
+echo "$x X + $jtinitama + $jtinitoni <br>";
+echo "X = $y : $x = $z <br>";
+echo "umur tama : $z <br>";
+$tni = $z-$jtinitama;
+echo "umur tini = $tni <br>";
+$tno = 16;
+echo "umur toni = $tno";
+
+?>
